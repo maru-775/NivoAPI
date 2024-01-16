8559c50d15e165cf1fc4952c3f8fd56bf3c0a181
maru-775 <bouzida.marouane@gmail.com>
2024-01-16 08:42:19 +0100

    feat: new function to parse the administrations based on departements

--- main.rb
+++ main.rb
@@ -1,2 +1,16 @@
-# frozen_string_literal: true
+require 'json'
+file_path = "assets/api-lannuaire-administration.json"
 
+json_data = File.read(file_path)
+data = JSON.parse(json_data)
+departments = ['16', '24', '33', '31', '32', '64', '65', '79', '86']
+
+data.each do |administration|
+  if administration['adresse']
+    address_array = JSON.parse(administration['adresse'])
+    code_postal = address_array.find do |code|
+      departments.any? { |departement| code['code_postal'].start_with?(departement) }
+    end
+    puts code_postal
+  end
+end
